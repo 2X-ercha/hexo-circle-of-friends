9f1b5c3a5f1b6e977b3cbe83ee7e7d6102ca708d
noionion <noionion@outlook.com>
2021-05-19 12:43:52 +0800

    update link_repeat

--- run.py
+++ run.py
@@ -91,19 +91,16 @@ def get_post(friend_poor):
         nonlocal total_count
         nonlocal post_poor
         nonlocal error_count
-        error = False
+        error = True
         try:
             total_count += 1
-            error, post_poor = sitmap_get(item, post_poor)
-            
-            if error:
-                print("-----------获取sitemap信息失败，采取主页爬取策略----------")
-            
             for themelinkfun in themes:
-                if error:
+                if not error:
                     break
                 error = themelinkfun.get_last_post(item, post_poor)
-                
+            if error:
+                print("-----------获取主页信息失败，采取sitemap策略----------")
+                error, post_poor = sitmap_get(item, post_poor)
         except Exception as e:
             print('\n')
             print(item, "运用主页及sitemap爬虫爬取失败！请检查")
